932d1d61b0c3617ec6fd2a22bf598fa85cf9628e
Gabriel Nobrega <gabrielnobrega63@gmail.com>
2025-06-24 12:47:24 -0300

    fixing readme width on mobile

--- app/routes/readme/readme.tsx
+++ app/routes/readme/readme.tsx
@@ -7,11 +7,11 @@ export default function Readme() {
   const html = marked(README);
   return (
     <section className='flex w-full items-center justify-center p-4'>
-      <main className='flex w-full max-w-3xl flex-col items-center justify-center py-40'>
-        <article className='prose prose-sm prose-neutral dark:prose-invert'>
+      <main className='flex w-screen max-w-3xl flex-col items-center justify-center px-4 py-[40vh]'>
+        <article className='prose prose-sm lg:prose prose-neutral dark:prose-invert w-full'>
           <div dangerouslySetInnerHTML={{ __html: html }} />
         </article>
-        <Button asChild variant='secondary' className='mt-4 w-full text-xs'>
+        <Button asChild variant='secondary' className='mt-4 px-8 text-xs'>
           <Link to='/'>Voltar para o dashboard</Link>
         </Button>
       </main>
